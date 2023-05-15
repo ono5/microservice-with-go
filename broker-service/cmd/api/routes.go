cc42812dc3e4b5078fa482bf4995c5168754d7de
ono5 <kuroneko_head_lord@yahoo.co.jp>
2023-05-16 05:35:36 +0900

    create auth functions

--- broker-service/cmd/api/routes.go
+++ broker-service/cmd/api/routes.go
@@ -24,8 +24,7 @@ func (app *Config) routes() http.Handler {
 
 	mux.Post("/", app.Broker)
 
-	mux.Use(middleware.Heartbeat("/ping"))
+	mux.Post("handle", app.HandleSubmission)
 
-	mux.Post("/authenticate", app.Authenticate)
 	return mux
 }
